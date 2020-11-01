e4e8dca36daff9188268d7ce529df32b9689a23b
Fernanda Manzano <manzanofernandadaniela@gmail.com>
2020-10-31 21:17:49 -0300

    login

--- VirtualCash/Login.Designer.cs
+++ VirtualCash/Login.Designer.cs
@@ -47,7 +47,7 @@
             this.pictureBox1.Image = global::VirtualCash.Properties.Resources.VCFOND_removebg_preview;
             this.pictureBox1.Location = new System.Drawing.Point(-13, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(363, 266);
+            this.pictureBox1.Size = new System.Drawing.Size(350, 266);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
